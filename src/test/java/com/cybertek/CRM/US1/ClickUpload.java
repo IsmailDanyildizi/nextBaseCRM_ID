4317f2f9f3b87f3da384189e531bc4a4462b95bc
unknown <idanyildizi@hotmail.com>
2020-05-26 19:07:07 +0200

    add ClickUpload.java

---
+++ src/test/java/com/cybertek/CRM/US1/ClickUpload.java
@@ -0,0 +1,12 @@
+package com.cybertek.CRM.US1;
+
+import org.openqa.selenium.WebDriver;
+
+public class ClickUpload {
+    WebDriver driver;
+
+    public static void main(String[] args) {
+        System.out.println("yoo");
+    }
+
+}
