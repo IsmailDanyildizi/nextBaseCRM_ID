e8265e470b05d39e78872a2fb521f5abaeb52db0
unknown <idanyildizi@hotmail.com>
2020-06-06 15:38:45 +0200

    update ClickUpload.java

--- src/test/java/com/cybertek/CRM/US1/ClickUpload.java
+++ src/test/java/com/cybertek/CRM/US1/ClickUpload.java
@@ -6,7 +6,9 @@ public class ClickUpload {
     WebDriver driver;
 
     public static void main(String[] args) {
-        System.out.println("yooa");
+        System.out.println("yoo");
+        System.out.println("123" +
+                "");
     }
 
 }
